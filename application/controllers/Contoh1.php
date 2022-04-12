986c84aebf89f8599f68a12616e731c6e07864e1
caxxtle <salmanmufid97@gmail.com>
2022-04-12 09:53:18 +0700

    git commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya asep supra
+            Saya tingga di daerah cileleuyan
+            olah raga yang saya sukai adalah
+            Bersepedah dipagi hari bertemu anya geraldine";
+    }
+}
